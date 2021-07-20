3b8e014278d7117caeac32080981d0a3443f39e2
Julius Peter Oketayot <juliuspetero360@gmail.com>
2021-07-21 01:14:18 +0300

    Rename project

--- src/main/java/com/mojagap/mojanode/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/mojagap/mojanode/infrastructure/configuration/BeanConfiguration.java
@@ -1,6 +1,5 @@
 package com.mojagap.mojanode.infrastructure.configuration;
 
-
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/test/java/com/mojagap/mojanode/AppTests.java
+++ src/test/java/com/mojagap/mojanode/AppTests.java
@@ -8,10 +8,7 @@ class AppTests {
 
     @Test
     void contextLoads() {
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(AppTests.class.getSimpleName());
-        System.out.println(AppTests.class.getName());
-        System.out.println(AppTests.class.getCanonicalName());
+
     }
 
 }
